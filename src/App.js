b501b4cd5fa5f7a9c5db865f0ce87ec2bb5e556f
wrapperik <159801009+wrapperik@users.noreply.github.com>
2025-03-11 18:52:25 +0200

    Errors Fix 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './App.css';
 import Navbar from './Components/navbar';
-import Searchbar from './Components/searchbar';
 import Compare from './Components/compare';
 import Home from './Components/home';
 import Timeline from './Components/timeline';
@@ -15,7 +14,6 @@ function App() {
       <div className="App">
         <header className="App-header">
           <Navbar />
-  
         </header>
         <main>
           <Routes>
